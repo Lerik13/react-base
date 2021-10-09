c3ae5fded49ddcb8c0512855b049754fd6271258
Valeria <lerik13@gmail.com>
2021-10-09 08:03:34 -0600

    use hook 'useEffect' for loading posts during Mount phase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
@@ -6,20 +6,26 @@ import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/MyModal/MyModal';
 import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
+import PostService from './API/PostService';
 
 function App() {
-	const [posts, setPosts] = useState([
-		{id: 1, title: 'JavaScript', body: 'Description'},
-		{id: 2, title: 'zzz 2', body: 'aaa'},
-		{id: 3, title: 'bbb 3', body: 'bbb'}
-	]);
+	const [posts, setPosts] = useState([]);
 	const [filter, setFilter] = useState({sort: '', query: ''})
 	const [modal, setModal] = useState(false)
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+	useEffect(() => {
+		fetchPosts()
+	}, []);
+	
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost])
 		setModal(false)
+	}
+
+	async function fetchPosts() {
+		const posts = await PostService.getAll()
+		setPosts(posts)
 	}
 	// Get post from child component
 	const removePost = (post) => {
@@ -28,6 +34,7 @@ function App() {
 
 	return (
 		<div className="App">
+			<button onClick={fetchPosts}>GET POSTS</button>
 			<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
 				Create new Post
 			</MyButton>
@@ -39,7 +46,7 @@ function App() {
 				filter={filter}
 				setFilter={setFilter} 
 			/>
-			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts about JS:" />
+			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts:" />
 		</div>
 	);
 }
