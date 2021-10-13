d8120ba098c9388cc2cb0de53503c54d62e05d05
Valeria <lerik13@gmail.com>
2021-10-13 12:03:45 -0600

    fixed behavior: during update page '/posts:id' redirectto 'posts'

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from 'react';
 import {Route, Switch, Redirect} from "react-router-dom";
 import { AuthContext } from '../context';
 import { privateRoutes, publicRoutes } from '../router';
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
-	const {isAuth, setIsAuth} = useContext(AuthContext);
+	const {isAuth, isLoading} = useContext(AuthContext);
+	if (isLoading) {
+		return <Loader/>
+	}
 
 	return (
 		isAuth
